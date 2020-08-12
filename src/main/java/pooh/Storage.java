ed862e2ac317d4353468376c86b7b127c025102f
DenisViskov <denisviskov16@gmail.com>
2020-08-12 12:47:31 +0300

    update

---
+++ src/main/java/pooh/Storage.java
@@ -0,0 +1,38 @@
+package pooh;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Класс реализует ...
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 12.08.2020
+ */
+@ThreadSafe
+public class Storage implements Store<String> {
+
+    private final Queue<String> store;
+
+    public Storage(Queue<String> store) {
+        this.store = store;
+    }
+
+    public Storage() {
+        this.store = new ConcurrentLinkedQueue<>();
+    }
+
+    @Override
+    public String get() {
+        return store.poll();
+    }
+
+    @Override
+    public void add(String some) {
+        store.offer(some);
+    }
+}
---
+++ src/main/java/pooh/Store.java
@@ -0,0 +1,13 @@
+package pooh;
+
+/**
+ * Интерфейс ...
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 12.08.2020
+ */
+public interface Store<T> {
+    T get();
+    void add(T some);
+}
